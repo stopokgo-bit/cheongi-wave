b540e186c62be7f6e807a42515c65275f7b83cbf
stopokgo-bit <stop.ok.go@gmail.com>
2026-04-23 20:12:26 +0900

    Update saju.js

--- api/saju.js
+++ api/saju.js
@@ -1,4 +1,5 @@
 export default async function handler(req, res) {
+  // CORS 헤더
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');

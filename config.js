a748ad69db96f20f1e8d4d713e9a17f086dd534f
stopokgo-bit <stop.ok.go@gmail.com>
2026-04-30 00:58:36 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -1,1 +1,10 @@
+export const config = { runtime: 'edge' };
 
+export default function handler() {
+  return new Response(JSON.stringify({
+    supabaseUrl: process.env.SUPABASE_URL || '',
+    supabaseKey: process.env.SUPABASE_ANON_KEY || '',
+  }), {
+    headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' }
+  });
+}
